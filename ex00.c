b6ea6bde5246aa31b3addbbf6be7e5bd2c5008c6
virgi0710 <virginiadandrea2008@gmail.com>
2024-05-06 14:55:35 +0000

    first commit

---
+++ ex00.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void maria()
+  {
+    printf("ila per me sono 8 mesi");
+  }
+int main()
+{
+    maria();
+    printf("ilaria vai da franci franci che sei disperata\n");
+    printf("federico mi fai impazzire in senso negativo\n");
+    printf("F&F vade retro\nila dobbiamo divertirci in sardegna\n");
+}
---
+++ vivivi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+ {
+    //VARIABILI//
+
+    int a; //numeri interi
+    float b = 2.587654; //numeri con virgola
+    double c = 2.68465230986453254658769; //numeri con virgola + precisi
+
+    a = 2.7;
+    printf("a-10 = %d\n", a - 10); //identificazione per int: %d
+    printf("b+22: %f\n", b + 22);
+    printf("c+918: %lf\n", c + 918);
+    printf("a: %d\n", a);
+    printf("somma: %f\n", a+b);
+    float differenza = a-b;
+    printf("differenza: %f\n", differenza);
+    float divisione = a/b;
+    printf("divisione: %f\n", divisione);
+    float moltiplicazione = a*b;
+    printf("moltiplicazione: %f\n", moltiplicazione);
+    //SOMMA? DIFFERENZA?//è possibile effettuare operazioni
+ }
